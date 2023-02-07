38d053b8bed355426e9b46d3e6bdfe2732c222d4
VejdiBURAK <burakvejdin@gmail.com>
2023-02-07 18:42:17 +0300

    Day-3
    Commit of day3

---
+++ Day-3/day3.js
@@ -0,0 +1,28 @@
+/*
+copyWithin() --> adds values in a specified range of the array to the array relative to its target value
+1.Parameter -> Target
+2.Parameter -> Starting Index
+3.Parameter -> End Index
+
+fill() --> Replace elements in the specified range of the array with value
+1.Parameter -> Value
+2.Parameter -> Starting Index
+3.Parameter -> End Index
+
+concat()--> combines min 2 array and returns an array. var arr1 = arr2.concat(arr3,..,..)
+
+join() --> Returns a string data using the parameter given to it as a separator. 
+
+valueOf() --> add reference of array to new array. var arr1 = arr2.valueOf(). Every change on arr2 now effects arr1
+
+indexOf() --> returns first found index of element given as parameter
+
+lastIndexOf() --> returns last found index of element given as parameter
+
+reverse() --> returns reversed array.
+
+sort() --> returns sorted array by unicode ranks
+
+
+
+*/
