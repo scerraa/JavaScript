226f493aecfdeebb8c1fe41833c9ab9ec0d93332
VejdiBURAK <burakvejdin@gmail.com>
2023-02-07 14:29:16 +0300

    Day-2
    Commit of day2

---
+++ Day-2/day2.js
@@ -0,0 +1,22 @@
+/*
+startsWith() --> returns true or false based on input parameter if starts with it. variable.startsWith("Java")
+endsWith() -->  returns true or false based on input parameter if ends with it. variable.endssWith("Java")
+match() --> returns input parameter if its includes in variable if not returns null. variable.match("Java")
+replace() --> takes 2 parameter. 1 text to be replaced. 2 text what is it replace to. Replaces first matching.
+var1.replace(/PHP/g,"JavaScript") -g look for all text. -i ignores lower or uppercase of content
+split() --> returns an array that splitted from variable based on input parameter. var1.split(" ") 
+charCodeAt() --> returns unicode of given index as parameter.
+fromCharCode() --> returns carecter of given unicode number as parameter var a= String.fromCharCode(65,68,98)
+encodeUri() --> returns encoded UTF8 value of variable that called.  a.encodeUri()
+decodeUri() --> reverse encoded utf8 value
+sub() and sup() --> Makes character down or up in the string
+isArray() --> returns boolean based on parameter Array.isArray([1,2,3])
+
+array1.pop() --> return last element of array and removes from array
+array1.shift() --> return first element of array and removes from array
+array1.push("") --> add element(s) to array. returns length of array's new version
+array1.unshift() --> add element(s) to top of the array. returns length of array's new version
+array1.splice(a,b,c) --> removes element(s) from array returns removed element(s). a->starting index b->how many elements to be removed.
+c->element(s) to be added to array after a index.
+
+*/
